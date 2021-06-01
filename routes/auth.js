fc9dac33692ad369722c87c0956ff226a0b60834
Srijan Das <srijandas18.sd@gmail.com>
2021-06-01 16:22:38 +0530

    deploy

--- routes/auth.js
+++ routes/auth.js
@@ -20,22 +20,26 @@ router.post("/register", async (req, res) => {
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
 //LOGIN
 router.post("/login", async (req, res) => {
   try {
+    console.log("\n\nuser: ", req.body);
     const user = await User.findOne({ email: req.body.email });
     !user && res.status(404).json("user not found");
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password)
-    !validPassword && res.status(400).json("wrong password")
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("wrong password");
 
-    res.status(200).json(user)
+    return res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err)
+    return res.status(500).json(err);
   }
 });
 

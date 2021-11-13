3724e559f2313e5faab88e4eb86868c68d8d0fa6
Srijan Das <srijandas18.sd@gmail.com>
2021-11-13 20:51:22 +0530

    increased refresh toke expiry time

--- routes/auth.js
+++ routes/auth.js
@@ -57,7 +57,7 @@ router.post("/login", async (req, res) => {
       { email: user.email },
       process.env.REFRESH_TOKEN_SECRET,
       {
-        expiresIn: "1d",
+        expiresIn: "7d",
       }
     );
 

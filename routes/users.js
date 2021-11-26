309177860b04b491cfaabc614de6f7aeb859e0a3
unknown <srijandas18.sd@gmail.com>
2021-11-26 17:28:38 +0530

    now you can connect to other users

--- routes/users.js
+++ routes/users.js
@@ -60,7 +60,7 @@ router.get("/friends/:userId", async (req, res) => {
   try {
     const user = await User.findById(req.params.userId);
     const friends = await Promise.all(
-      user.following.map((friendId) => {
+      user.connections.map((friendId) => {
         return User.findById(friendId);
       })
     );
@@ -75,47 +75,90 @@ router.get("/friends/:userId", async (req, res) => {
   }
 });
 
-//follow a user
-
-router.put("/:id/follow", async (req, res) => {
+//send connection request to a user
+router.put("/:id/connect", async (req, res) => {
   if (req.body.userId !== req.params.id) {
     try {
       const user = await User.findById(req.params.id);
-      const currentUser = await User.findById(req.body.userId);
-      if (!user.followers.includes(req.body.userId)) {
-        await user.updateOne({ $push: { followers: req.body.userId } });
-        await currentUser.updateOne({ $push: { following: req.params.id } });
-        res.status(200).json("user has been followed");
+      if (!user.connections.includes(req.body.userId)) {
+        if (!user.connectRequests.includes(req.body.userId))
+          await user.updateOne({ $push: { connectRequests: req.body.userId } });
+        res.status(200).json("sent connection request");
       } else {
-        res.status(403).json("you allready follow this user");
+        res.status(403).json("you are already connected to this user");
       }
     } catch (err) {
       res.status(500).json(err);
     }
   } else {
-    res.status(403).json("you cant follow yourself");
+    res.status(403).json("you cant connect to yourself");
   }
 });
 
-//unfollow a user
-
-router.put("/:id/unfollow", async (req, res) => {
+// confirm connection request
+router.put("/:id/accept", async (req, res) => {
   if (req.body.userId !== req.params.id) {
     try {
       const user = await User.findById(req.params.id);
-      const currentUser = await User.findById(req.body.userId);
-      if (user.followers.includes(req.body.userId)) {
-        await user.updateOne({ $pull: { followers: req.body.userId } });
-        await currentUser.updateOne({ $pull: { following: req.params.id } });
-        res.status(200).json("user has been unfollowed");
+      if (user.connectRequests.includes(req.body.userId)) {
+        await user.updateOne({
+          $pull: { connectRequests: req.body.userId },
+          $push: { connections: req.body.userId },
+        });
+        await User.findByIdAndUpdate(req.body.userId, {
+          $push: { connections: req.params.id },
+        });
+        res.status(200).json("connection request confirmed");
       } else {
-        res.status(403).json("you dont follow this user");
+        res.status(403).json("you dont have any connection request");
       }
     } catch (err) {
       res.status(500).json(err);
     }
   } else {
-    res.status(403).json("you cant unfollow yourself");
+    res.status(403).json("you cant connect to yourself");
+  }
+});
+
+// reject connection request
+router.put("/:id/reject", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      if (user.connectRequests.includes(req.body.userId)) {
+        await user.updateOne({
+          $pull: { connectRequests: req.body.userId },
+        });
+        res.status(200).json("connection request rejected");
+      } else {
+        res.status(403).json("you dont have any connection request");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you cant connect to yourself");
+  }
+});
+
+// disconnect from a user
+router.put("/:id/disconnect", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.connections.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { connections: req.body.userId } });
+        await currentUser.updateOne({ $pull: { connections: req.params.id } });
+        res.status(200).json("you disconnected from this user");
+      } else {
+        res.status(403).json("you are not connected to this user");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you cant disconnected from yourself");
   }
 });
 
